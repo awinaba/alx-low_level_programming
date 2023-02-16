b42ff28b18889d294887d0afb5d21622108910bf
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-16 03:25:44 -0800

    Add advanced task file 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/**
+ * main - prints exactly 'and that piece of art is useful\"' -
+ * Dora Korpar, 2015-10-19
+ *
+ * Return: 1
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
