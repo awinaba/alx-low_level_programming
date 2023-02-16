97161c4c7b9f3917ac6ebfe298879690ed1121bd
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-16 02:31:03 -0800

    Add task file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/**
+ * main - A program that prints exactly \"Programming is like building
+ * a multilingual puzzle
+ *
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle\n\0";
+
+	puts(str);
+
+	return (0);
+}
