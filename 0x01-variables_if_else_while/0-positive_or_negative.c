0df19775eff1ff5f472ea66cd06b4f3060ce48a0
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-17 03:02:33 -0800

    Add task file 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print whether the number stored in the variable n is
+ * positive or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+
+	return (0);
+}
