5fb348413dd8e08ee2d40e8e35036fa4dd377669
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-21 03:12:42 -0800

    Add task file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  Entry point
+ * Description: prints _putchar, followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char _putchar[] = "_putchar";
+
+	puts(_putchar);
+	putchar('\n');
+
+	return (0);
+}
