32f01d13f069d80471f046d9983165c748d054ec
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-21 06:14:18 -0800

    Add _puthcar function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,10 +4,12 @@
  * print_last_digit - prints the last digit of a number
  * @n : number for which the last digit is to be printed
  *
- * Return: Always 0
+ * Return: Always @n
  */
 
 int print_last_digit(int n)
 {
-	return (n % 10);
+	n %= 10;
+	_putchar(n)
+	return (n);
 }
