d79dadf4e1a6c04f48296dab7c8f105788f353ed
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-21 06:41:28 -0800

    Find the absolute of the number before finding the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,12 +14,12 @@ int print_last_digit(int n)
 		n *= -1;
 		n %= 10;
 		_putchar(n + '0');
-		return (n + '0');
+		return (n);
 	}
 	else
 	{
 		n %= 10;
 		_putchar(n + '0');
-		return (n + '0');
+		return (n);
 	}
 }
