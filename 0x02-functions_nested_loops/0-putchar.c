76f0000b7622de2546ad328a1997b0398759b9c2
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-21 04:01:55 -0800

    Change print function from puts to _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * main -  Entry point
@@ -10,10 +9,15 @@
 
 int main(void)
 {
-	char _putchar[] = "_putchar";
-
-	puts(_putchar);
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
