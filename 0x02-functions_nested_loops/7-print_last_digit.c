d246f6819b02c52b268e66e3a05b9628d0a6bc9e
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-21 06:11:20 -0800

    Add task file 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n : number for which the last digit is to be printed
+ *
+ * Return: Always 0
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
