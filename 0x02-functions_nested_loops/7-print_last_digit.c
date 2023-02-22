8e7adfa0af6e97d787f9408bec16a3d2740a7e9c
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-22 00:47:59 -0800

    Change absolute conversion algorithm

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,15 +4,22 @@
  * print_last_digit - prints the last digit of a number
  * @n : number for which the last digit is to be printed
  *
- * Return: Always @n
+ * Return: Always last digit of @n
  */
 
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n < 0)
 		n *= -1;
 
-	_putchar('0' + (n % 10));
+	last_digit = n % 10;
 
-	return (n % 10);
+	if (last_digit < 0)
+		last_digit *= -1
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
