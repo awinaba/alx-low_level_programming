f579e9a945862039b4aa53ab759d4643c4389386
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-22 02:43:02 -0800

    Add advanced task file 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the first 50 fibonacci numbers,
+ * starting with 1 and 2
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number_one = 1;
+	int number_two = 2;
+	long int fibonacci number = number_one + number_two;
+	int counter;
+
+	for (counter = 3; counter <= 50; counter++)
+	{
+		if (counter == 50)
+		{
+			printf("%lu\n", fibonacci_number);
+		}
+		else
+		{
+			printf("%lu, ", fibonacci_number);
+		}
+
+		number_one = number_two;
+		number_two = fibonacci_number;
+	}
+
+	return (0);
+}
