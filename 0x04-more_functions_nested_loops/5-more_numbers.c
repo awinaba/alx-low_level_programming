a30136ba983544db7dc3cec4208de1b4d2672d93
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-23 06:43:27 -0800

    Change while loop to for loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,19 +10,18 @@
 void more_numbers(void)
 {
 	int counter;
-	int number = 0;
+	int number;
 
 	for (counter = 0; counter <= 10; counter++)
 	{
-		while (number <= 14)
+		for (number = 0; number <= 14; number++)
 		{
 			if (number >= 10)
 			{
 				_putchar((number / 10) + '0');
 			}
 			_putchar((number % 10) + '0');
-			number++;
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
