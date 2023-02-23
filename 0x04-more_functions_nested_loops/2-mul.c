d3dfd109574eb99a8513fb7d5083b771053816f6
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-23 05:32:01 -0800

    Add task file 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: multiplier
+ * @b: multiplier
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
