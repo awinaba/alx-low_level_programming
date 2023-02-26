6934e5bf00068a42a176e2473922f8290f2da361
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-26 05:54:54 -0800

    Change argument name from size to n

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,12 +12,13 @@ void print_diagonal(int n)
 	int symbol;
 	int indent;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
 	for (symbol = 1; symbol <= n; symbol++)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
 		for (indent = 1; indent < symbol; indent++)
 		{
 			_putchar(' ');
