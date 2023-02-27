170ae2511355514d23ed98f502fb9df76b4a1970
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-27 05:41:42 -0800

    Add task file 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int parameter
+ * an updates the value it points to to 98
+ * @*n: pointer to an int parameter
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	return *n = 98;
+}
+
