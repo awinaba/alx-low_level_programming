74538cc33f7ed8e8c9f61881ae08ff0b0543dc4f
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-02-27 05:53:08 -0800

    Add task file 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#inlcude "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	int *ptr_c = &c
+	int *ptr_a = &a;
+	int *ptr_b = &b;
+
+	ptr_c = *ptr_b;
+	ptr_b = *ptr_a;
+	ptr_a = ptr_c;
+}
