d98e512d8843a49e4aace024afb0136bf2257890
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-03-02 03:29:19 -0800

    Change algoritm

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,22 +10,16 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int j;
 
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] !='\0' && j < n)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest[i] = src[i];
 	}
-
-	dest[i] = '\0';
+	
+	for( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 
 	return (dest);
 }
