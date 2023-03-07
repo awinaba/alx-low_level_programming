eb1f264cbe0546395562d46003476a50751418af
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-03-07 05:05:30 -0800

    Add task file 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: 2d array of char
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int y, x;
+
+	y = 0;
+	while (y < 8)
+	{
+		x = 0;
+		while (x < 8)
+		{
+			_putchar(a[y][x]);
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+}
