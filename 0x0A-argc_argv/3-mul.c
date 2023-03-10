1e1a794572330f598165c1bc0cbbccaf800235de
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-03-10 08:37:21 -0800

    Add task file 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: multiplies two numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int product = 1;
+	int counter;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (counter = 1; counter < argc; counter++)
+	{
+		product *= atoi(argv[counter]);
+	}
+
+	printf("%d\n", product);
+
+	return (0);
+}
