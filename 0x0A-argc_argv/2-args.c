71a85c9c0c85fac37f7790c9b1ddd14f6898132d
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-03-10 08:21:27 -0800

    Add task file 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints all arguments a program receives
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int counter;
+
+	for (counter = 0 ; counter == argc; counter++)
+	{
+		printf("%s\n", argv[counter]);
+	}
+
+	return (0);
+}
