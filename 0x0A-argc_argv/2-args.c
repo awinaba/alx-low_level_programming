2ada3efb9e31ee21173ddfbca156a4d466b8975c
Abdul-Mumin Awinaba <awinabaam@outlook.com>
2023-03-10 08:23:17 -0800

    Add task file 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	int counter;
 
-	for (counter = 0 ; counter == argc; counter++)
+	for (counter = 0 ; counter < argc; counter++)
 	{
 		printf("%s\n", argv[counter]);
 	}
